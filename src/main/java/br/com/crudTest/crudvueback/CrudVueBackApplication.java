fdfaf11beb47c1f94587bb7afd8301bd4d74cddd
Rogerio Junior <rogerio@gmail.com>
2019-09-10 20:24:34 -0300

    first commit

---
+++ src/main/java/br/com/crudTest/crudvueback/CrudVueBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.crudTest.crudvueback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudVueBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudVueBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crudTest/crudvueback/CrudVueBackApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.crudTest.crudvueback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudVueBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
