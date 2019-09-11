fbc3f28995793a60d4cb7f280b246ae60addb076
Rogerio Junior <rogeriopcjr@gmail.com>
2019-09-11 14:58:50 -0300

    commit

--- src/main/java/br/com/crudTest/crudvueback/CrudVueBackApplication.java
+++ src/main/java/br/com/crudTest/crudvueback/CrudVueBackApplication.java
@@ -2,6 +2,15 @@ package br.com.crudTest.crudvueback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class CrudVueBackApplication {
@@ -9,5 +18,22 @@ public class CrudVueBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudVueBackApplication.class, args);
 	}
+
+
+//	// Fix the CORS errors
+//	@Bean
+//	public FilterRegistrationBean simpleCorsFilter() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+//		// *** URL below needs to match the Vue client URL and port ***
+//		config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+//		config.setAllowedMethods(Collections.singletonList("*"));
+//		config.setAllowedHeaders(Collections.singletonList("*"));
+//		source.registerCorsConfiguration("/**", config);
+//		FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter((CorsConfigurationSource) source));
+//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return bean;
+//	}
 
 }
